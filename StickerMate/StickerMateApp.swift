dd675275df5a233346044f36cbe843669f549104
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2022-09-17 00:03:43 +0200

    Initial Commit

---
+++ StickerMate/StickerMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerMateApp.swift
+//  StickerMate
+//
+//  Created by Miká Kruschel on 17.09.22.
+//
+
+import SwiftUI
+
+@main
+struct StickerMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
