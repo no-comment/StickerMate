6a797ce870358bfe3784e054568332b153c70e09
Cameron Shemilt <31541782+cameronshemilt@users.noreply.github.com>
2022-09-17 08:50:54 +0200

    oops

---
+++ StickerMate/CustomSheet.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSheet.swift
+//  StickerMate
+//
+//  Created by Cameron Shemilt on 17.09.22.
+//
+
+import SwiftUI
+
+struct CustomSheet: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CustomSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSheet()
+    }
+}
--- StickerMate/views/ProfileCard.swift
+++ StickerMate/views/ProfileCard.swift
@@ -30,6 +30,7 @@ struct ProfileCard: View {
                 }
                 .shadow(radius: 7)
         }
+        .shee
     }
 }
 
