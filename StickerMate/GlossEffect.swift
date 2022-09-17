3118c7c77639812016aa991672046f1b14eef363
Cameron Shemilt <31541782+cameronshemilt@users.noreply.github.com>
2022-09-17 08:44:48 +0200

    add gloss

--- StickerMate/GlossEffect.swift
+++ StickerMate/GlossEffect.swift
@@ -8,33 +8,36 @@
 import SwiftUI
 
 struct GlossEffect: ViewModifier {
-    @State private var x: CGFloat = 0
-    @State private var yOffset: CGFloat = 0
+    private let size: CGFloat = 100
+    @State private var rad: Double = 0
+    @State private var rotate: Bool = false
     
     func body(content: Content) -> some View {
-        GeometryReader { geo in
-            let size = geo.size.width
-
             content
                 .overlay {
                     Circle()
                         .fill(
-                            RadialGradient(colors: [.red, .clear],
+                            RadialGradient(colors: [.white.opacity(0.5), .clear],
                                            center: .center,
                                            startRadius: 1,
-                                           endRadius: size)
+                                           endRadius: size/1.5)
                         )
                         .frame(width: 2*size, height: 2*size)
-                        .offset(y: geo.size.width/3)
-                        .rotationEffect(.radians(x))
+                        .offset(y: size/3)
+                        .rotationEffect(.radians(rad))
+                        .rotationEffect(.degrees(225))
                 }
+                .clipShape(Rectangle())
+                .rotation3DEffect(.degrees(rotate ?  2 : -2), axis: (x: 1, y: 0, z:  0))
+                .animation(.timingCurve(0.45, 0.45, 1, 1, duration: 10).repeatForever(autoreverses: true), value: rotate)
+                .rotation3DEffect(.degrees(rotate ? 2 : -2), axis: (x: 0, y: 1, z:  0))
+                .animation(.timingCurve(0.45, 0.45, 1, 1, duration: 10).repeatForever(autoreverses: true).delay(5.0), value: rotate)
                 .onAppear {
-                    withAnimation(.linear(duration: 15).repeatForever(autoreverses: false)) {
-                        x = 2 * (.pi)
+                    withAnimation(.linear(duration: 20).repeatForever(autoreverses: false)) {
+                        rad = 2 * (.pi)
                     }
+                    rotate.toggle()
                 }
-            
-        }
     }
 }
 
@@ -46,8 +49,12 @@ extension View {
 
 struct Glosseffect_Previews: PreviewProvider {
     static var previews: some View {
-        StickerBadge(image: Image("sticker.example.profile.1"), isEvent: false)
-            .glossEffect()
-            .frame(width: 100, height: 100)
+        LazyVGrid(columns: [.init(.adaptive(minimum: 100))]) {
+            StickerBadge(image: Image("sticker.example.profile.2"), isEvent: true)
+                .glossEffect()
+            StickerBadge(image: Image("sticker.example.profile.2"), isEvent: false)
+                .glossEffect()
+        }
+        .padding()
     }
 }
--- StickerMate/views/StickerBadge.swift
+++ StickerMate/views/StickerBadge.swift
@@ -12,15 +12,26 @@ struct StickerBadge: View {
     let isEvent: Bool
     
     var body: some View {
+        if isEvent {
+            imageView
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+        } else {
+            imageView
+                .clipShape(Circle())
+        }
+    }
+    
+    var imageView: some View {
         image
             .resizable()
-            .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fit)
-            .border(.red)
+            .aspectRatio(1, contentMode: .fit)
+            .glossEffect()
     }
 }
 
 struct StickerBadge_Previews: PreviewProvider {
     static var previews: some View {
-        StickerBadge(image: Image("sticker.example.profile.1"), isEvent: false)
+        StickerBadge(image: Image("sticker.example.profile.2"), isEvent: true)
+            .frame(width: 100)
     }
 }
