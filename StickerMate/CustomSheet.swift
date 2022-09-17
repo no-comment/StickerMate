9228d5a1ddb85f81d1a88b2832629582d0c50e41
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2022-09-17 09:54:13 +0200

    Transition customsheet

--- StickerMate/CustomSheet.swift
+++ StickerMate/CustomSheet.swift
@@ -11,20 +11,31 @@ struct CustomSheet<Content: View>: View {
     private let content: Content
     @Binding private var binding: Bool
 
+    @State private var showingOverlay = false
+
     init(item: Binding<Bool>, content: Content) {
         self._binding = item
         self.content = content
     }
-    
+
     var body: some View {
-        if binding {
-            ZStack {
-                Color.black.opacity(0.3)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        binding = false
+        ZStack {
+            Color.black.opacity(binding ? 0.3 : 0)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    binding = false
+                    showingOverlay = false
+                }
+                .onChange(of: binding, perform: { newValue in
+                    guard newValue else { return }
+                    withAnimation {
+                        showingOverlay = true
                     }
+                })
+
+            if showingOverlay {
                 content
+                    .transition(.asymmetric(insertion: .opacity.combined(with: .offset(y: 50)), removal: .opacity))
             }
         }
     }
@@ -33,14 +44,15 @@ struct CustomSheet<Content: View>: View {
 struct CustomSheetModifier<CustomContent: View>: ViewModifier {
     private let customContent: CustomContent
     private let binding: Binding<Bool>
-    
+
     init(binding: Binding<Bool>, content: CustomContent) {
         self.binding = binding
         self.customContent = content
     }
-    
+
     func body(content: Content) -> some View {
-            content
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .overlay {
                 CustomSheet(item: binding, content: customContent)
             }
@@ -48,17 +60,28 @@ struct CustomSheetModifier<CustomContent: View>: ViewModifier {
 }
 
 extension View {
-    func customSheet(_ isPresented: Binding<Bool>, @ViewBuilder content: () -> some View) -> some View {
+    func customSheet(isPresented: Binding<Bool>, @ViewBuilder content: () -> some View) -> some View {
         modifier(CustomSheetModifier(binding: isPresented, content: content()))
+    }
+}
+
+private struct Preview: View {
+    @State private var shows = false
+    var body: some View {
+        Button("Show") {
+            withAnimation {
+                shows.toggle()
+            }
+        }
+        .customSheet(isPresented: $shows) {
+            StickerCard()
+                .padding()
+        }
     }
 }
 
 struct CustomSheet_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .customSheet(.constant(true)) {
-                ProfileCard()
-                    .padding()
-            }
+        Preview()
     }
 }
