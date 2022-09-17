0c5c1fa7d912040968aee642244e6ba383a98c93
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2022-09-17 12:16:30 +0200

    Imported firebase

--- StickerMate/StickerMateApp.swift
+++ StickerMate/StickerMateApp.swift
@@ -5,10 +5,22 @@
 //  Created by Miká Kruschel on 17.09.22.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct StickerMateApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
