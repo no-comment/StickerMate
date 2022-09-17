8695934f27e7579ab38e4d18f16b8b99b6dde33d
Cameron Shemilt <31541782+cameronshemilt@users.noreply.github.com>
2022-09-17 11:42:04 +0200

    clean up gloss

--- StickerMate/GlossEffect.swift
+++ StickerMate/GlossEffect.swift
@@ -10,13 +10,8 @@ import SwiftUI
 struct GlossEffect: ViewModifier {
     private let duration: CGFloat = 14
     private let size: CGFloat = 140
-    private let offset: CGFloat = 50
     @State private var rad: Double = 0
     @State private var rotate: Bool = false
-    
-    var sinus: CGFloat {
-        sin(rad)
-    }
     
     func body(content: Content) -> some View {
             content
@@ -38,7 +33,7 @@ struct GlossEffect: ViewModifier {
                 .animation(.timingCurve(0.45, 0.45, 1, 1, duration: duration/2).repeatForever(autoreverses: true), value: rotate)
                 .rotation3DEffect(.degrees(rotate ? 10 : -10), axis: (x: 0, y: 1, z:  0))
                 .animation(.timingCurve(0.45, 0.45, 1, 1, duration: duration/2).repeatForever(autoreverses: true).delay(duration/4), value: rotate)
-                .shadow(color: .gray, radius: 20, x: sinus * offset, y: sin(rad) * offset)
+                .shadow(color: .gray, radius: 14)
                 .onAppear {
                     withAnimation(.linear(duration: duration).repeatForever(autoreverses: false)) {
                         rad = 2 * (Double.pi)
@@ -56,13 +51,6 @@ extension View {
 
 struct Glosseffect_Previews: PreviewProvider {
     static var previews: some View {
-//        LazyVGrid(columns: [.init(.adaptive(minimum: 100))]) {
-//            StickerBadge(image: Image("sticker.example.event.1"), isEvent: true)
-//                .glossEffect()
-//            StickerBadge(image: Image("sticker.example.profile.2"), isEvent: false)
-//                .glossEffect()
-//                .shadow(radius: 5)
-//        }
         StickerBadge(image: Image("sticker.example.event.1"), isEvent: true)
             .frame(width: 140)
             .glossEffect()
