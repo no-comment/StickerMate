8c6a8341a6ac6dca8b0f145da71c0ad546174732
Cameron Shemilt <31541782+cameronshemilt@users.noreply.github.com>
2022-09-17 09:23:08 +0200

    Update CustomSheet.swift

--- StickerMate/CustomSheet.swift
+++ StickerMate/CustomSheet.swift
@@ -7,14 +7,58 @@
 
 import SwiftUI
 
-struct CustomSheet: View {
+struct CustomSheet<Content: View>: View {
+    private let content: Content
+    @Binding private var binding: Bool
+
+    init(item: Binding<Bool>, content: Content) {
+        self._binding = item
+        self.content = content
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        if binding {
+            ZStack {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        binding = false
+                    }
+                content
+            }
+        }
+    }
+}
+
+struct CustomSheetModifier<CustomContent: View>: ViewModifier {
+    private let customContent: CustomContent
+    private let binding: Binding<Bool>
+    
+    init(binding: Binding<Bool>, content: CustomContent) {
+        self.binding = binding
+        self.customContent = content
+    }
+    
+    func body(content: Content) -> some View {
+            content
+            .overlay {
+                CustomSheet(item: binding, content: customContent)
+            }
+    }
+}
+
+extension View {
+    func customSheet(_ isPresented: Binding<Bool>, @ViewBuilder content: () -> some View) -> some View {
+        modifier(CustomSheetModifier(binding: isPresented, content: content()))
     }
 }
 
 struct CustomSheet_Previews: PreviewProvider {
     static var previews: some View {
-        CustomSheet()
+        ContentView()
+            .customSheet(.constant(true)) {
+                ProfileCard()
+                    .padding()
+            }
     }
 }
